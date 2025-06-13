80738eb4cc60397e33338e5aaa87e783c57902b8
Rodion <marpleway@Noutbuk-Rodion.local>
2025-06-13 17:41:44 +0300

    sorry i remember that i need to commit

---
+++ src/locales/ru.js
@@ -0,0 +1,13 @@
+export default {
+  translation: {
+    errors: {
+      invalidURL: 'Ссылка должна быть валидным URL',
+      notContainRSS: 'Ресурс не содержит валидный RSS',
+      alreadyExist: 'RSS уже существует',
+      networkError: 'Ошибка сети',
+      required: 'Не должно быть пустым',
+    },
+
+    success: 'RSS успешно загружен',
+  },
+};
